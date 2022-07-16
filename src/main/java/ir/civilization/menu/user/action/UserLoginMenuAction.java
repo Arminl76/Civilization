a2866dbe2d6aeb300b00902437f94c153a2f0532
armin <armin.lohrrasbi.1998@gmail.com>
2022-07-16 21:26:25 +0430

    Add game turn validation in login

--- src/main/java/ir/civilization/menu/user/action/UserLoginMenuAction.java
+++ src/main/java/ir/civilization/menu/user/action/UserLoginMenuAction.java
@@ -4,7 +4,10 @@ package ir.civilization.menu.user.action;
 import ir.civilization.dao.user.UserDao;
 import ir.civilization.dto.UserDTO;
 import ir.civilization.exception.BadCredentialsException;
+import ir.civilization.holder.GameContext;
+import ir.civilization.holder.GameHolder;
 import ir.civilization.menu.AbstractMenuAction;
+import ir.civilization.model.Civilization;
 import ir.civilization.model.user.User;
 import ir.civilization.security.AuthenticatedUserHolder;
 
@@ -29,6 +32,14 @@ public class UserLoginMenuAction extends AbstractMenuAction<UserDTO> {
     public void takeAction(UserDTO userDTO) {
         Optional<User> user = userDao.findByUsernameAndPassword(userDTO.getUsername(), userDTO.getPassword());
         if (user.isPresent()) {
+            // validate turn user 
+            GameContext context = GameHolder.getCreatedContext();
+            Civilization civilization = context.getCivilization();
+            if (civilization != null) {
+                User cUser = civilization.getUser();
+                if (cUser != null && !user.get().equals(cUser))
+                    throw new IllegalArgumentException("It's not your turn!");
+            }
             authenticatedUserHolder.setPrinciple(user.get());
             System.out.println("You logged in successfully!");
         } else
